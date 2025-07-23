3e849110a2a29b142aca99fa1ca02e4c24d0058f
BadCoder2 <nathanh1804@gmail.com>
2025-07-22 22:46:58 -0500

    Fix turn logic for 'L' command, add turn command test for all, update current task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Overall goal: create a 3d visualization of a 3x3x3 Rubik's Cube in Python
-# Current task: Test the newly implemented (i.e., untested) turn functions
+# Current task: FIX the newly implemented (i.e., broken) turn functions. The current test succeeds up until the 'D' turn, which is broken.
 # TODO in future: Implement the 'M', 'E', and 'S' turns, which are middle slice turns
 
 # Config
@@ -107,8 +107,8 @@ class Cube:
                 temp = self.cube['L'][0][i]
                 self.cube['L'][0][i] = self.cube['F'][0][i]
                 self.cube['F'][0][i] = self.cube['R'][0][i]
-                self.cube['R'][0][i] = self.cube['B'][0][2-i]
-                self.cube['B'][0][2-i] = temp
+                self.cube['R'][0][i] = self.cube['B'][0][i]
+                self.cube['B'][0][i] = temp
 
             # Step 2: Rotate the U face's corners
             temp = self.cube['U'][0][0]
@@ -199,7 +199,6 @@ def test_cube_turn_r_basic():
     print(final_cubestring)
     assert final_cubestring == 'WWGWWGWWGOOOOOOOOOGGYGGYGGYRRRRRRRRRWBBWBBWBBYYBYYBYYB'
     print("Basic test passed.")
-
 def test_cube_turn_r_adv():
     cubestring_of_solved_cube_turned_u = 'WWWWWWWWWGGGOOOOOORRRGGGGGGBBBRRRRRROOOBBBBBBYYYYYYYYY'
     cur_cube = Cube()
@@ -226,8 +225,46 @@ def test_cube_turn_l_basic():
     print(final_cubestring)
     assert final_cubestring == 'BWWBWWBWWOOOOOOOOOWGGWGGWGGRRRRRRRRRBBYBBYBBYGYYGYYGYY'
     print("Basic test passed.")
+def test_cube_turn_l_adv():
+    cubestring_of_solved_cube_turned_u = 'WWWWWWWWWGGGOOOOOORRRGGGGGGBBBRRRRRROOOBBBBBBYYYYYYYYY'
+    cur_cube = Cube()
+    cur_cube.set_cubestring(cubestring_of_solved_cube_turned_u)
+    print(cur_cube.get_cubestring())
+    cur_cube.turn('L')
+    print(cur_cube.get_cubestring())
+    cur_cube.turn('L')
+    print(cur_cube.get_cubestring())
+    assert cur_cube.get_cubestring() == 'YWWYWWYWWOOOOOOGGGBRRBGGOGGBBBRRRRRROOGBBGBBRWYYWYYWYY'
+    print("Advanced test phase 1 passed.")
+    cur_cube.turn('L')
+    cur_cube.turn('L')
+    print(cur_cube.get_cubestring())
+    assert cur_cube.get_cubestring() == cubestring_of_solved_cube_turned_u
+    print("Advanced test phase 2 passed.")
+
+def test_cube_all_turns():
+    cur_cube = Cube()
+    original_cubestring = cur_cube.get_cubestring()
+    print(original_cubestring)
+    cur_cube.turn('R')
+    print(cur_cube.get_cubestring())
+    cur_cube.turn('L')
+    print(cur_cube.get_cubestring())
+    cur_cube.turn('U')
+    print(cur_cube.get_cubestring())
+    cur_cube.turn('D')
+    print(cur_cube.get_cubestring())
+    cur_cube.turn('F')
+    print(cur_cube.get_cubestring())
+    cur_cube.turn('B')
+    final_cubestring = cur_cube.get_cubestring()
+    print(final_cubestring)
+    assert final_cubestring == 'YRYWWWYOYBGGBOGBBGOWROGROYRGBBGRBGGBRWORBORYOWRWYYYWOW'
+
 
 if __name__ == "__main__" and test_mode == True:
     #test_cube_turn_r_basic()
     #test_cube_turn_r_adv()
-    test_cube_turn_l_basic()
+    #test_cube_turn_l_basic()
+    #test_cube_turn_l_adv()
+    test_cube_all_turns()
